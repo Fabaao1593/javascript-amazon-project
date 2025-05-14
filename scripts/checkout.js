712d7eaabcf0ea45722eeb307c4a7f3515998ae0
Fabio de Oliveira Alves <fabio_slpk@hotmail.com>
2025-05-13 21:03:43 -0300

    update delivery options

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,7 @@ import {
   calculateCartQuantity,
   removeFromCart,
   addToCart,
+  updateDeliveryOption,
 } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
@@ -101,7 +102,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
 
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
     html += ` 
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option"
+      data-product-id="${matchingProduct.id}"
+      data-delivery-option-id="${deliveryOption.id}">
           <input
           type="radio"
           ${isChecked ? "checked" : ""}
@@ -150,3 +153,10 @@ document.querySelectorAll(".js-update-quatity-link").forEach((link) =>
     addToCart(productId);
   })
 );
+
+document.querySelectorAll(".js-delivery-option").forEach((element) => {
+  element.addEventListener("click", () => {
+    const { productId, deliveryOptionId } = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId);
+  });
+});
